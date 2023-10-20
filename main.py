dd68903d02cadcc13a9cd3a0576f0b1c5416ed59
ram.pandey <ram.pandey@hurix.com>
2023-10-20 17:07:11 +0530

    feat: completed project

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 with open("books/frankenstein.txt", "r") as f:
     file_contents = f.read()
     letter_dict = {}
+    word_count = len(file_contents.split())
     file_contents = file_contents.replace(" ", "")
     file_contents = file_contents.replace("\n", "")
     file_contents = file_contents.replace("\t", "")
@@ -11,4 +12,13 @@ with open("books/frankenstein.txt", "r") as f:
         else:
             letter_dict[letter.lower()] = 1
 
-    print(letter_dict)
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{word_count} words by default")
+
+    sorted_dict = sorted(
+        letter_dict.items(), key=lambda character: character[1], reverse=True
+    )
+
+    for letter_data in sorted_dict:
+        if letter_data[0].isalpha():
+            print(f"character {letter_data[0]} is repeated {letter_data[1]} times")
